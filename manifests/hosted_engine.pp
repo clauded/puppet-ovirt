849bcb98ed9a4a3afb91aa8efc68ca1b42877804
Claude Durocher <claude.durocher@cspq.gouv.qc.ca>
2016-05-16 13:56:15 -0400

    test hosted engine setup

--- manifests/hosted_engine.pp
+++ manifests/hosted_engine.pp
@@ -1,28 +1,29 @@
 # == Class: ovirt::hosted_engine
-class ovirt::hosted_engine(
-  $engine_admin_password          = $ovirt::engine_admin_password,
-  $hosted_engine_fqdn             = $ovirt::hosted_engine_fqdn,
-  $hosted_engine_service_package  = $ovirt::hosted_engine_service_package,
+class ovirt::hosted_engine (
+  $engine_admin_password                = $ovirt::engine_admin_password,
+  $hosted_engine_fqdn                   = $ovirt::hosted_engine_fqdn,
+  $hosted_engine_service_package        = $ovirt::hosted_engine_service_package,
   $hosted_engine_service_package_ensure = $ovirt::hosted_engine_service_package_ensure,
-  $hosted_engine_setup_conf_d      = $ovirt::hosted_engine_setup_conf_d,
-  $hosted_engine_setup_conf_d_name = $ovirt::hosted_engine_setup_conf_d_name,
-  $hosted_engine_services          = $ovirt::hosted_engine_services,
-  $hosted_engine_service_ensure    = $ovirt::hosted_engine_service_ensure,
-  $hosted_engine_service_enable    = $ovirt::hosted_engine_service_enable,
-  $hosted_engine_mac_address       = $ovirt::hosted_engine_mac_address,
-  $hosted_engine_gateway           = $ovirt::hosted_engine_gateway,
-  $hosted_engine_num_cpu           = $ovirt::hosted_engine_num_cpu,
-  $hosted_engine_mb_ram            = $ovirt::hosted_engine_mb_ram,
-  $hosted_engine_gb_storage        = $ovirt::hosted_engine_gb_storage,
+  $hosted_engine_setup_conf_d           = $ovirt::hosted_engine_setup_conf_d,
+  $hosted_engine_setup_conf_d_name      = $ovirt::hosted_engine_setup_conf_d_name,
+  $hosted_engine_services               = $ovirt::hosted_engine_services,
+  $hosted_engine_service_ensure         = $ovirt::hosted_engine_service_ensure,
+  $hosted_engine_service_enable         = $ovirt::hosted_engine_service_enable,
+  $hosted_engine_mac_address            = $ovirt::hosted_engine_mac_address,
+  $hosted_engine_gateway                = $ovirt::hosted_engine_gateway,
+  $hosted_engine_num_cpu                = $ovirt::hosted_engine_num_cpu,
+  $hosted_engine_mb_ram                 = $ovirt::hosted_engine_mb_ram,
+  $hosted_engine_gb_storage             = $ovirt::hosted_engine_gb_storage,
 ) inherits ovirt::node {
 
   $conf = "${hosted_engine_setup_conf_d}/${hosted_engine_setup_conf_d_name}"
 
   package { $hosted_engine_service_package:
     ensure => $hosted_engine_service_package_ensure,
+    #notify => Exec['run hosted-engine-setup'],
   }
 
-  file {$hosted_engine_setup_conf_d:
+  file { $hosted_engine_setup_conf_d:
     ensure  => directory,
     require => Package[$hosted_engine_service_package],
   }
@@ -35,8 +36,22 @@ class ovirt::hosted_engine(
     content => template('ovirt/etc/otopi.conf.d/hosted-engine.erb'),
     before  => Service[$ovirt::node::node_service_name],
     require => [
-                  Package[$hosted_engine_service_package],
-                  File[$hosted_engine_setup_conf_d], ],
+      Package[$hosted_engine_service_package],
+      File[$hosted_engine_setup_conf_d],
+    ],
+  }
+
+  if $engine_admin_password {
+    file { 'ovirt-admin-password':
+      path    => $ovirtadminpassfile,
+      owner   => 'root',
+      group   => 'kvm',
+      mode    => '0640',
+      content => "[environment:default]\nOVESETUP_CONFIG/adminPassword=str:${engine_admin_password}\n"
+      require => Package[$engine_service_package],
+    }
+  } else {
+    fail('engine_admin_password cannot be undefined')
   }
 
   service { $hosted_engine_services:
@@ -45,4 +60,19 @@ class ovirt::hosted_engine(
     require => Package[$hosted_engine_service_package],
   }
 
+  # This is configured to automatically run ovirt-hosted-engine-setup
+  # if you've set ensure=> latest on th engine, thus
+  # performing your post install update step
+  exec { 'run hosted-engine-setup':
+    command     => 'ovirt-hosted-engine-setup',
+    path        => '/usr/bin/:/bin/:/sbin:/usr/sbin',
+    logoutput   => true,
+    timeout     => 1850,
+    refreshonly => true,
+    before      => Service[$hosted_engine_services],
+    require     => [
+      File['hosted pre-seed answers'],
+      File['ovirt-admin-password'],
+    ],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,38 +3,38 @@ class ovirt::params {
   $node_service_package        = 'vdsm'
   $node_service_package_ensure = 'installed'
 
-  $node_service_name        = 'vdsmd'
-  $node_service_ensure      = 'running'
-  $node_service_enabled     = true
-  $node_updateFirewall      = true
+  $node_service_name    = 'vdsmd'
+  $node_service_ensure  = 'running'
+  $node_service_enabled = true
+  $node_updateFirewall  = true
 
-  $engine_service_package         = 'ovirt-engine'
-  $engine_service_package_ensure  = 'installed'
+  $engine_service_package        = 'ovirt-engine'
+  $engine_service_package_ensure = 'installed'
 
-  $engine_service_name      = 'ovirt-engine'
-  $engine_service_ensure    = 'running'
-  $engine_service_enable    = false
+  $engine_service_name   = 'ovirt-engine'
+  $engine_service_ensure = 'running'
+  $engine_service_enable = false
 
   $engine_setup_conf_d      = '/etc/ovirt-engine-setup.conf.d'
   $engine_setup_answers     = '/var/lib/ovirt-engine/setup/answers/*.conf'
   $engine_setup_conf_d_name = '999-puppet.conf'
   $engine_application_mode  = 'both'
 
-  $engine_db_username       = 'engine'
-  $engine_db_password       = $::uuid
-  $engine_db_hostname       = 'localhost'
-  $engine_db_dbname         = 'engine'
-  $engine_db_port           = '5432'
+  $engine_db_username = 'engine'
+  $engine_db_password = $::uuid
+  $engine_db_hostname = 'localhost'
+  $engine_db_dbname   = 'engine'
+  $engine_db_port     = '5432'
 
   $engine_admin_password          = undef
   $engine_admin_password_filename = '888-adminpass.conf'
 
-  $engine_iso_domain_name      = 'ISO_DOMAIN'
-  $engine_iso_domain_location  = '/var/lib/exports/iso'
+  $engine_iso_domain_name     = 'ISO_DOMAIN'
+  $engine_iso_domain_location = '/var/lib/exports/iso'
   if $::domain {
-    $engine_iso_domain_acl     = "${::fqdn}(rw) *.${::domain}(ro,insecure)"
+    $engine_iso_domain_acl    = "${::fqdn}(rw) *.${::domain}(ro,insecure)"
   } else {
-    $engine_iso_domain_acl     = "${::fqdn}(rw) *(ro,insecure)"
+    $engine_iso_domain_acl    = "${::fqdn}(rw) *(ro,insecure)"
   }
 
   if $::organization {
@@ -57,7 +57,7 @@ class ovirt::params {
   $hosted_engine_service_package_ensure = 'installed'
   $hosted_engine_setup_conf_d           = '/etc/otopi.conf.d'
   $hosted_engine_setup_conf_d_name      = '999-puppet-hosted-engine.conf'
-  $hosted_engine_services = ['ovirt-ha-agent.service', 'ovirt-ha-broker.service']
-  $hosted_engine_service_ensure = 'running'
-  $hosted_engine_service_enable = true
+  $hosted_engine_services               = ['ovirt-ha-agent.service', 'ovirt-ha-broker.service']
+  $hosted_engine_service_ensure         = 'running'
+  $hosted_engine_service_enable         = true
 }
