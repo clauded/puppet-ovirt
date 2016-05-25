6eb3dbdc663be4bdf537d5dcb1db84da9abf5962
Claude Durocher <claude.durocher@cspq.gouv.qc.ca>
2016-05-25 15:42:39 -0400

    add vdsm-cli to node installation

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,7 @@ class ovirt::params {
   $ovirt_engine_appliance_file          = '/root/ovirt-engine-appliance-3.6-20160524.1.el7.centos.noarch.rpm'
   $ovirt_engine_appliance_ensure        = 'absent'
 
-  $node_service_package                 = 'vdsm'
+  $node_service_package                 = [ 'vdsm', 'vdsm-cli' ]
   $node_service_package_ensure          = 'installed'
   $node_service_name                    = 'vdsmd'
   $node_service_ensure                  = 'running'
@@ -24,7 +24,7 @@ class ovirt::params {
 
   $hosted_engine_service_package        = 'ovirt-hosted-engine-setup'
   $hosted_engine_service_package_ensure = 'installed'
-  $hosted_engine_service_name           = ['ovirt-ha-agent.service', 'ovirt-ha-broker.service']
+  $hosted_engine_service_name           = [ 'ovirt-ha-agent.service', 'ovirt-ha-broker.service' ]
   $hosted_engine_service_ensure         = 'running'
   $hosted_engine_service_enable         = false
   $hosted_engine_setup_conf_d           = '/etc/otopi.conf.d'
