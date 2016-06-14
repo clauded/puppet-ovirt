8799eeb8c4030b5dd8d7eed9a8ad8a96b95d9c01
Claude Durocher <claude.durocher@cspq.gouv.qc.ca>
2016-06-14 09:45:26 -0400

    change var name

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,9 +2,9 @@
 
 class ovirt::repo (
 
-  $ovirt_repo_manage       = $ovirt::ovirt_repo_manage,
-  $ovirt_repo_package_name = $ovirt::ovirt_repo_package_name,
-  $ovirt_repo_version      = $ovirt::ovirt_repo_version,
+  $ovirt_repo_manage  = $ovirt::ovirt_repo_manage,
+  $ovirt_repo_url     = $ovirt::ovirt_repo_url,
+  $ovirt_repo_version = $ovirt::ovirt_repo_version,
 
 ) inherits ovirt {
 
@@ -12,12 +12,12 @@ class ovirt::repo (
     command => "yum -y update && touch /etc/puppet/yum_update.done",
     path    => '/usr/bin:/usr/sbin:/bin:/sbin',
     creates => "/etc/puppet/yum_update.done",
-    timeout => '300',
+    timeout => '600',
     before  => Package["ovirt-release${ovirt_repo_version}"],
   }
   package { "ovirt-release${ovirt_repo_version}":
     ensure => present,
-    source => $ovirt_repo_package_name
+    source => $ovirt_repo_url
   }
 
 }
