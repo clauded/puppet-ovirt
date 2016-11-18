074dfc679136569c61994292ba0c14be403b5739
Claude Durocher <claude.durocher@cspq.gouv.qc.ca>
2016-11-18 10:34:48 -0500

    code refactoring

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,7 +17,7 @@ class ovirt::params {
   $engine_service_package_ensure        = 'installed'
   $engine_service_name                  = 'ovirt-engine'
   $engine_service_ensure                = 'running'
-  $engine_service_enabled               = true
+  $engine_service_enabled               = false
   $engine_setup_conf_d                  = '/etc/ovirt-engine-setup.conf.d'
   $engine_answers_file                  = 'puppet:///modules/ovirt/engine_answers.conf'
   $engine_run_engine_setup              = false
@@ -26,7 +26,7 @@ class ovirt::params {
   $hosted_engine_service_package_ensure = 'installed'
   $hosted_engine_service_name           = [ 'ovirt-ha-agent.service', 'ovirt-ha-broker.service' ]
   $hosted_engine_service_ensure         = 'running'
-  $hosted_engine_service_enabled        = true
+  $hosted_engine_service_enabled        = false
   $hosted_engine_setup_conf_d           = '/etc/otopi.conf.d'
   $hosted_engine_answers_file           = 'puppet:///modules/ovirt/hosted_engine_answers.conf'
   $hosted_engine_run_engine_setup       = false
